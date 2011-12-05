ac1c17bb853df7d5f4e4ee39a9070e26d48da920
Adam Ernst <adamernst@cosmicsoft.net>
2011-12-05 09:07:43 -0500

    Introduce a cacheURLs array to prevent race conditions related to checking the on-disk image cache.
    If a cache check is initiated, then canceled with cancelForDelegate: before a new cache check is started with a different URL but the same delegate, the ongoing disk cache check would still message the delegate despite being canceled by cancelForDelegate:. This is because it only checked to see if the delegate was in the cacheDelegates array; it had been removed by cancelForDelegate:, but added back by the new unrelated cache check. cacheURLs keeps track of which specific URLs are actually requested by the delegate. If the URL from a completed disk cache check does not match the delegate in cacheDelegates, the delegate is not messaged (since we know that request was canceled).

--- SDWebImageManager.h
+++ SDWebImageManager.h
@@ -16,6 +16,7 @@
     NSMutableArray *downloadDelegates;
     NSMutableArray *downloaders;
     NSMutableArray *cacheDelegates;
+    NSMutableArray *cacheURLs;
     NSMutableDictionary *downloaderForURL;
     NSMutableArray *failedURLs;
 }
